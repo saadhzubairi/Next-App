14550b0c172a6d49b3d02f5fcaba4ba92343d5a6
Saad Zubairi <saadhzubairi@outlook.com>
2023-11-04 23:44:51 +0500

    initi

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,9 @@
-import Link from "next/link";
-import ProductCard from "./components/productCard";
+
 
 export default function Home() {
   return (
     <main>
       <h1 className="">Hello World</h1>
-      <Link href="/users">to users</Link>
-      <ProductCard />
-      
     </main>
   )
 }
